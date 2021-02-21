7b71dd084ec8bb419fa1fe2a837b629b2c37dc05
caixc <965571656@qq.com>
2021-02-21 14:26:07 +0800

    53  最大子序和 贪心算法 遇到累加子序和<0直接丢弃

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -15,7 +15,8 @@ public class ArrayTest {
 //        System.out.println(i);
 
 
-
+        int[] arr = {8,2,4,7};
+        System.out.println( new ArrayTest().longestSubarray(arr, 4));;
     }
 
     /**
@@ -88,6 +89,7 @@ public class ArrayTest {
 
     /**
      * 数独判断
+     *
      * @param board
      * @return
      */
@@ -134,24 +136,24 @@ public class ArrayTest {
                 //行
                 if (lines[i].contains(c)) {
                     return false;
-                }else{
+                } else {
                     lines[i].add(c);
                 }
                 //列
                 if (rows[j].contains(c)) {
                     return false;
-                }else{
+                } else {
                     rows[j].add(c);
                 }
                 //栅格
                 int bdIndex = 0;
                 int line = i / 3;
                 int row = j / 3;
-                bdIndex = line + 3*row;
+                bdIndex = line + 3 * row;
 
                 if (bdList[bdIndex].contains(c)) {
                     return false;
-                }else{
+                } else {
                     bdList[bdIndex].add(c);
                 }
             }
@@ -160,7 +162,67 @@ public class ArrayTest {
 
     }
 
+    /**
+     * 绝对差不超过限制的最长连续子数组
+     * <p>
+     * 思路 移动窗口 从左向右 双指针
+     * 使用一个自排序 结构存储窗口内容
+     * 使用TreeMap  自平衡二叉树 从首尾获取到 最大值和最小值
+     * <p>
+     * right++  每次保存更新数据到 TreeMap
+     * TreeMap 最小值跟最大值 差 >limit 则left++ 直到符合条件
+     * 保存当前 left right 之间的距离到 maxLen
+     *
+     * @param nums
+     * @param limit
+     * @return
+     */
+    public int longestSubarray(int[] nums, int limit) {
+        int left = 0;
+        int right = 0;
+        int len = nums.length;
+        int res = 0;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        while (right < len) {
+            //存储 出现的数据 出现次数
+            //并且实现数据自动排序 （TreeMap 底层 自平衡二叉树）
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+            //窗口内最大最小值差 超出限制值
+            while (map.lastKey() - map.firstKey() > limit) {
+                map.put(nums[left], map.get(nums[left])-1); //左侧元素次数-1
+                if (map.getOrDefault(nums[left],0) == 0) { //该元素没有了 直接清空
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+            //每一次满足条件的移位 记录当前长度
+            res = Math.max(res, right - left + 1);
+            right++;
+        }
 
+        return res;
+    }
 
+    /**
+     * 最大子序和
+     * Given  -2 1 -3 4 -1 2 1 -7 4
+     * Return   4 -1 2 1 result =6
+     * 贪心解法  遇到连续和小于0则丢弃 因为再加会越来越小
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int result = Integer.MIN_VALUE;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count += nums[i];
+            result = Math.max(result, count);
+            //遇到小于等于0 连续和 直接丢弃
+            if (count <= 0) {
+                count = 0;
+            }
+        }
+        return result;
+    }
 
 }
