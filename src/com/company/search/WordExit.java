02ece3e28f491546788d2460e60938b1d865d7fe
Caixc <caixc@anchnet.com>
2021-02-22 18:12:03 +0800

    79 单词搜索 轮训 迭代 回溯

--- src/com/company/search/WordExit.java
+++ src/com/company/search/WordExit.java
@@ -1,7 +1,7 @@
 package com.company.search;
 
 /**
- *  搜索相关
+ * 搜索相关
  */
 public class WordExit {
 
@@ -9,15 +9,15 @@ public class WordExit {
      * 查询单词是否连续存在在数组中
      * board =
      * [
-     *   ['A','B','C','E'],
-     *   ['S','F','C','S'],
-     *   ['A','D','E','E']
+     * ['A','B','C','E'],
+     * ['S','F','C','S'],
+     * ['A','D','E','E']
      * ]
-     *
+     * <p>
      * 给定 word = "ABCCED", 返回 true
      * 给定 word = "SEE", 返回 true
      * 给定 word = "ABCB", 返回 false
-     *
+     * <p>
      * 字母不能重复使用
      * 贪吃蛇
      *
@@ -26,47 +26,68 @@ public class WordExit {
      * @return
      */
     public boolean exist(char[][] board, String word) {
-        int h = board.length;
-        int w = board[0].length;
-        boolean[][] visited = new boolean[h][w]; //存储每个位置是否访问过
 
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                boolean flag = check(board, visited, i, j, word, 0); //从word的第0个数字开始
+        int column = board.length;
+        int row = board[0].length;
+        //定义网格每个字符被访问状态
+        boolean[][] visited = new boolean[column][row];
+
+        //网格中每一个元素 遍历一次， pos 从 0 - word 长度 进行状态判断
+        for (int i = 0; i < column; i++) {
+            for (int j = 0; j < row; j++) {
+                boolean flag = check(board, visited, word, i, j, 0);
                 if (flag) {
                     return true;
                 }
             }
         }
+
+
         return false;
     }
 
-
-    private boolean check(char[][] board, boolean[][] visited, int i, int j, String s, int pos) {
-        if (board[i][j] != s.charAt(pos)) {
+    /**
+     * 检查 s 的第 pos 个元素 是否符合条件 ，并向下一个pos 推移 直到遍历完整个串
+     *
+     * @param board
+     * @param visited
+     * @param word
+     * @param i
+     * @param j
+     * @param pos
+     * @return
+     */
+    private boolean check(char[][] board, boolean[][] visited, String word, int i, int j, int pos) {
+        //定义出口
+        if (board[i][j] != word.charAt(pos)) { // 当前位置数 与 word 的pos 相等
             return false;
-        } else if (pos == s.length() - 1) {
+        } else if (pos == word.length() - 1) { //并且 索引是目标最后一个pos  ，表明匹配成功
             return true;
         }
+        // c1 ==c2 看下一个数据
         visited[i][j] = true;
-        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};   //四个方向 每个方向 坐标增加值
+        int[][] directions = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
         boolean result = false;
-        for (int[] dir : directions) {
-            int newi = i + dir[0];
-            int newj = j + dir[1];
-            if (newi >= 0 && newi < board.length && newj >= 0 && newj < board[0].length) {
-                if (!visited[newi][newj]) {
-                    boolean flag = check(board, visited, newi, newj, s, pos + 1);
-                    if (flag) {
-                        result = true;
-                        break;
-                    }
+        for (int[] direction : directions) {
+            //获取下一格的数据
+            int newi = i + direction[0];
+            int newj = j + direction[1];
+            if (newi < 0 || newj < 0 || newi >= board.length || newj >= board[0].length) { //超出边界
+                continue;
+            }
+            if (!visited[newi][newj]) { //下一个位置未被访问  访问
+                boolean check = check(board, visited, word, newi, newj, pos + 1); //后面的符合条件
+                if (check) { //因为要回溯 修改状态 所以这里不能直接return
+                    result = true;
+                    break;
                 }
             }
         }
-        visited[i][j] = false; //回溯 改回标志位
+        visited[i][j] = false; //回溯  置回状态
         return result;
     }
 
-
 }
+
+
+
