9b15fe4a9ddfac4153c67728b349f3cd7358a5b8
Caixc <caixc@anchnet.com>
2021-02-23 23:18:29 +0800

    128 最长连续数列 hash +暴力破解 +去除非必要循环

---
+++ src/com/company/hash/HashTest.java
@@ -0,0 +1,33 @@
+package com.company.hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashTest {
+
+    //连续最长序列  原数组不需要连续
+    public int longestConsecutive(int[]nums){
+        Set<Integer> num_set = new HashSet<>();
+
+        for (int num : nums) { //去重
+            num_set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : num_set) {  //遍历
+            if (!num_set.contains(num - 1)) {  //当前数 -1 不存在在集合中 再遍历
+                                                //如果当前数-1 在集合中 说明 又 num-1 来遍历这一次 跟本次重复了
+                int currentNum = num;
+                int currentStreak = 1;
+                while (num_set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+                longestStreak = Math.max(longestStreak, currentStreak);
+
+            }
+        }
+        return longestStreak;
+    }
+}
