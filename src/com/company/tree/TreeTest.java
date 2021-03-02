28c9a5515460e5d88be703face8a7ce7b9f5d936
Caixc <caixc@anchnet.com>
2021-03-02 09:05:34 +0800

    调整

--- src/com/company/tree/TreeTest.java
+++ src/com/company/tree/TreeTest.java
@@ -340,24 +340,7 @@ public class TreeTest {
     }
 
 
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
 
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
 
 
     /**
