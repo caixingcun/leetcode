13ba4f7ae34e02678651c77b5d402a9b97632ed2
Caixc <caixc@anchnet.com>
2021-03-05 15:59:45 +0800

    347 前k高频元素 hash + priorityQueue顶栈

---
+++ src/com/company/hash/TopKFrequent.java
@@ -0,0 +1,47 @@
+package com.company.hash;
+
+import java.util.*;
+
+public class TopKFrequent {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        //int[0] 元素 int[1] 元素频率
+        //queue为队列
+        Queue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int num = entry.getKey();
+            int count = entry.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            }else{
+                queue.offer(new int[]{num, count});
+            }
+        }
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = queue.poll()[0];
+        }
+        return result;
+
+    }
+
+
+}
