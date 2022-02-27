40445f52891e6f20282c3dcde5af3f8947083d86
caixc <965571656@qq.com>
2022-02-27 12:25:39 +0800

    leetcode 105 hashmap优化

--- src/com/company/leetcode/LeetCode105.java
+++ src/com/company/leetcode/LeetCode105.java
@@ -1,18 +1,25 @@
 package com.company.leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 105 已知二叉树的前序中序数组，请重建这个树
  */
 public class LeetCode105 {
+   static   Map<Integer, Integer> cacheMap = new HashMap<>();
+
     public static void main(String[] args) {
         int[] firstArr = {1, 2, 4, 5, 3, 6, 7};
         int[] middleArr = {4, 2, 5, 1, 6, 3, 7};
+
+        for (int i = 0; i < middleArr.length; i++) {
+            cacheMap.put(middleArr[i], i);
+        }
         TreeNode root = f(firstArr, 0, 6, middleArr, 0, 6);
 
     }
-
 
 
     public static final TreeNode f(int[] firstArr, int firstStart, int firstEnd, int[] middleArr, int middleStart, int middleEnd) {
@@ -23,13 +30,7 @@ public class LeetCode105 {
         //中序根节点赋值
         TreeNode root = new TreeNode(firstArr[firstStart]);
         //中序第一个数 位置
-        int middleIndex = middleStart;
-        for (int i = middleStart; i <= middleEnd; i++) {
-            if (middleArr[i] == root.val) {
-                middleIndex = i;
-                break;
-            }
-        }
+        int middleIndex = cacheMap.get(root.val);
         int leftCount = middleIndex - middleStart;
         int rightCount = middleEnd - middleIndex;
 
